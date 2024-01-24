49ac7a1608de315cdd42604bd4024be54ccae216
pcpluspt <pcplus.pt@gmail.com>
2024-01-24 16:20:17 +0000

    Versão base do portefólio

---
+++ Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ConfiguraçãoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AndreToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PortefólioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HobiesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProjetoAToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProjetoBToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AndreiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DuarteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FabricioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FilipeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FranciscoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.JoaoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeonardoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RicardoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TomasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PlaneamentoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.CadetBlue
+        Me.MenuStrip1.Font = New System.Drawing.Font("Segoe UI Black", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ConfiguraçãoToolStripMenuItem, Me.AndreToolStripMenuItem, Me.AndreiToolStripMenuItem, Me.DuarteToolStripMenuItem, Me.FabricioToolStripMenuItem, Me.FilipeToolStripMenuItem, Me.FranciscoToolStripMenuItem, Me.JoaoToolStripMenuItem, Me.LeonardoToolStripMenuItem, Me.RicardoToolStripMenuItem, Me.TomasToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ConfiguraçãoToolStripMenuItem
+        '
+        Me.ConfiguraçãoToolStripMenuItem.Name = "ConfiguraçãoToolStripMenuItem"
+        Me.ConfiguraçãoToolStripMenuItem.Size = New System.Drawing.Size(101, 20)
+        Me.ConfiguraçãoToolStripMenuItem.Text = "Configuração"
+        '
+        'AndreToolStripMenuItem
+        '
+        Me.AndreToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PlaneamentoToolStripMenuItem, Me.PortefólioToolStripMenuItem, Me.HobiesToolStripMenuItem, Me.ProjetoAToolStripMenuItem, Me.ProjetoBToolStripMenuItem})
+        Me.AndreToolStripMenuItem.Name = "AndreToolStripMenuItem"
+        Me.AndreToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.AndreToolStripMenuItem.Text = "Andre"
+        '
+        'PortefólioToolStripMenuItem
+        '
+        Me.PortefólioToolStripMenuItem.Name = "PortefólioToolStripMenuItem"
+        Me.PortefólioToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PortefólioToolStripMenuItem.Text = "Portefólio"
+        '
+        'HobiesToolStripMenuItem
+        '
+        Me.HobiesToolStripMenuItem.Name = "HobiesToolStripMenuItem"
+        Me.HobiesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.HobiesToolStripMenuItem.Text = "Hobies"
+        '
+        'ProjetoAToolStripMenuItem
+        '
+        Me.ProjetoAToolStripMenuItem.Name = "ProjetoAToolStripMenuItem"
+        Me.ProjetoAToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ProjetoAToolStripMenuItem.Text = "Projeto A"
+        '
+        'ProjetoBToolStripMenuItem
+        '
+        Me.ProjetoBToolStripMenuItem.Name = "ProjetoBToolStripMenuItem"
+        Me.ProjetoBToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ProjetoBToolStripMenuItem.Text = "Projeto B"
+        '
+        'AndreiToolStripMenuItem
+        '
+        Me.AndreiToolStripMenuItem.Name = "AndreiToolStripMenuItem"
+        Me.AndreiToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.AndreiToolStripMenuItem.Text = "Andrei"
+        '
+        'DuarteToolStripMenuItem
+        '
+        Me.DuarteToolStripMenuItem.Name = "DuarteToolStripMenuItem"
+        Me.DuarteToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.DuarteToolStripMenuItem.Text = "Duarte"
+        '
+        'FabricioToolStripMenuItem
+        '
+        Me.FabricioToolStripMenuItem.Name = "FabricioToolStripMenuItem"
+        Me.FabricioToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.FabricioToolStripMenuItem.Text = "Fabricio"
+        '
+        'FilipeToolStripMenuItem
+        '
+        Me.FilipeToolStripMenuItem.Name = "FilipeToolStripMenuItem"
+        Me.FilipeToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.FilipeToolStripMenuItem.Text = "Filipe"
+        '
+        'FranciscoToolStripMenuItem
+        '
+        Me.FranciscoToolStripMenuItem.Name = "FranciscoToolStripMenuItem"
+        Me.FranciscoToolStripMenuItem.Size = New System.Drawing.Size(75, 20)
+        Me.FranciscoToolStripMenuItem.Text = "Francisco"
+        '
+        'JoaoToolStripMenuItem
+        '
+        Me.JoaoToolStripMenuItem.Name = "JoaoToolStripMenuItem"
+        Me.JoaoToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.JoaoToolStripMenuItem.Text = "Joao"
+        '
+        'LeonardoToolStripMenuItem
+        '
+        Me.LeonardoToolStripMenuItem.Name = "LeonardoToolStripMenuItem"
+        Me.LeonardoToolStripMenuItem.Size = New System.Drawing.Size(76, 20)
+        Me.LeonardoToolStripMenuItem.Text = "Leonardo"
+        '
+        'RicardoToolStripMenuItem
+        '
+        Me.RicardoToolStripMenuItem.Name = "RicardoToolStripMenuItem"
+        Me.RicardoToolStripMenuItem.Size = New System.Drawing.Size(65, 20)
+        Me.RicardoToolStripMenuItem.Text = "Ricardo"
+        '
+        'TomasToolStripMenuItem
+        '
+        Me.TomasToolStripMenuItem.Name = "TomasToolStripMenuItem"
+        Me.TomasToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.TomasToolStripMenuItem.Text = "Tomas"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(99, 61)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(593, 299)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'PlaneamentoToolStripMenuItem
+        '
+        Me.PlaneamentoToolStripMenuItem.Name = "PlaneamentoToolStripMenuItem"
+        Me.PlaneamentoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PlaneamentoToolStripMenuItem.Text = "Planeamento"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LemonChiffon
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Portefólio do 2º Ano de Programador Informático"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ConfiguraçãoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AndreToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PortefólioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HobiesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProjetoAToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProjetoBToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AndreiToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DuarteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FabricioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FilipeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FranciscoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents JoaoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LeonardoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RicardoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TomasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PlaneamentoToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub ProjetoAToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProjetoAToolStripMenuItem.Click
+        System.Diagnostics.Process.Start("C:\000\exemplo\winformsapp1.exe")
+    End Sub
+
+    Private Sub PlaneamentoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PlaneamentoToolStripMenuItem.Click
+        Dim myProcess = New Process()
+        myProcess.StartInfo.UseShellExecute = True
+        myProcess.StartInfo.FileName = "https://www.canva.com/design/DAF58mt9cPo/MLuXMr_RXyzxvv3dn14erA/view?utm_content=DAF58mt9cPo&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+        myProcess.Start()
+    End Sub
+End Class
+
+
+
